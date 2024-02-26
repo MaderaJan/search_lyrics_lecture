52bfa355ea0e925ad2119b353893d781a0e2cd20
janmadera <gmaderajan@gmail.com>
2024-02-26 23:45:50 +0100

    2. Lecture - End

--- app/src/main/java/com/maderajan/muni/lyricsseach/ui/lyricslist/LyricsListFragment.kt
+++ app/src/main/java/com/maderajan/muni/lyricsseach/ui/lyricslist/LyricsListFragment.kt
@@ -9,7 +9,6 @@ import com.maderajan.muni.lyricsseach.databinding.FragmentLyricsListBinding
 
 class LyricsListFragment : Fragment() {
 
-    // TODO 6. Fragment binding + lateinit usage
     private lateinit var binding: FragmentLyricsListBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
